6e2fe3b0e0c142e6c05bceb0b58ba2767ec46ef3
JoseOtavioZ <joseotz97@gmail.com>
2023-05-17 22:28:28 -0300

    Mark1_17/05

---
+++ src/HelloSwing.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HelloSwing extends JFrame{
+    private JButton enviarMensagemButton;
+    public JPanel painelPrincipal;
+    private JTextField txtMensagem;
+
+    public HelloSwing() {
+    enviarMensagemButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(enviarMensagemButton,txtMensagem.getText());
+        }
+    });
+}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mudei na branch");
+
+        HelloSwing form = new HelloSwing();
+        form.setContentPane(form.painelPrincipal);
+        form.setTitle("Hello Panel");
+        form.setSize(500,500);
+        form.setVisible(true);
+
+
     }
 
 }
